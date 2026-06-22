4d647b8e193a538f78810483424e6c0aac4cbb80
curtcast <pcurtcast@gmail.com>
2026-06-22 15:05:28 +0800

    fix: update helm provider syntax with equals assignment operator for v3 compatibility

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,7 +7,7 @@ provider "kubernetes" {
 data "google_client_config" "default" {}
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = "https://${google_container_cluster.primary.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.cluster_ca_certificate)
