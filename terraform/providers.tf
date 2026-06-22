0bd81fb868cd337e04dfca7bb2ed7de1381f4ec3
curtcast <pcurtcast@gmail.com>
2026-06-22 15:14:29 +0800

    fix: resolve master_auth block list index syntax for helm and kubernetes providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,6 +10,6 @@ provider "helm" {
   kubernetes = {
     host                   = "https://${google_container_cluster.autopilot_cluster.endpoint}"
     token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(google_container_cluster.autopilot_cluster.master_auth.cluster_ca_certificate)
+    cluster_ca_certificate = base64decode(google_container_cluster.autopilot_cluster.master_auth[0].cluster_ca_certificate)
   }
 }
